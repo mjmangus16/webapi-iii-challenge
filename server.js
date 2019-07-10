6a95c46fdaea01ddc5c60a09f7e6b2d1cc7d0284
MMangus <mjmangus16@yahoo.com>
2019-07-10 14:51:58 -0400

    basic structure complete and server is running

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = 'express';
+const express = "express";
+
+const postsRouter = require("./posts/postRouter");
+const usersRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
+function logger(req, res, next) {}
 
-};
+server.use("/posts", postsRouter);
+server.user("/users", usersRouter);
 
 module.exports = server;
